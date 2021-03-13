061c190b2b5508d64715b9617cfcb4f260d92483
Kundan Singh <kdsingh008@gmail.com>
2021-03-13 13:31:33 +0530

    first commit

---
+++ myscript.js
@@ -0,0 +1,44 @@
+const URL = "https://covid19.mathdro.id/api";
+
+let app = angular.module("MyApp", []);
+
+app.controller("MyCtrl", ($scope, $http) => {
+  //this is controller
+
+  $scope.title = "Stay home stay safe";
+
+  console.log("app loaded");
+
+  $http.get(URL).then(
+    (response) => {
+      //success
+
+      console.log(response.data);
+
+      $scope.all_data = response.data;
+    },
+    (error) => {
+      //error
+      console.log(error);
+    }
+  );
+
+  //get country data
+
+  $scope.get_c_data = () => {
+    let country = $scope.c;
+    if (country == "") {
+        $scope.c_data=undefined;
+      return;
+    }
+    $http.get(`${URL}/countries/${country}`).then(
+      (response) => {
+          console.log(response.data);
+          $scope.c_data = response.data;
+      },
+      (error) => {
+          console.log(error);
+      }
+    );
+  };
+});
